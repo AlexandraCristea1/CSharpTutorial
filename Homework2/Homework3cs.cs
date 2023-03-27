954e753c2cb29e149e8cb29e40a98b741b8a06e3
AlexandraCristea1 <86708012+AlexandraCristea1@users.noreply.github.com>
2023-03-27 14:40:21 +0300

    Homework3

---
+++ Homework2/Homework3cs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Homework3cs
+    {
+        public int id = 123; 
+        public string name = "Alexandra";
+    }
+}
